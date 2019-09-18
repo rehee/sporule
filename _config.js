219f91cc65c8f539d9c13a77afbe1da30114a5ec
hao <haleluohao@gmail.com>
2019-09-18 15:06:03 +0100

    updated the config to include google analytics

--- _config.js
+++ _config.js
@@ -3,10 +3,10 @@ const Config = {
     url:"https://www.sporule.com",
     description: "Sporule is a micro blog site",
     keywords:"micro blog",
-    logo:"https://i.imgur.com/MrRLOC4.png",
+    logo:"https://avatars1.githubusercontent.com/u/3833026?s=460&v=4",
     disqusShortname:"yourdisqusshortname",
-    postPerPage:8,
-    googleAnaltics:""
+    postPerPage:10,
+    googleAnaltics:"UA-114032457-1"
 }
 
 module.exports = Config;
