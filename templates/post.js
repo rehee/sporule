eb1ca3e2beee3f72b9a980464b16fd762fcab77a
hao <haleluohao@gmail.com>
2019-09-18 21:58:34 +0100

    updated posts

--- templates/post.js
+++ templates/post.js
@@ -3,47 +3,65 @@ import React from "react"
 const PostTemplate = (props) => {
     return (
         <React.Fragment>
-            <aside className="toc">
-                {props.toc}
-            </aside>
-            <div id="main" role="main" className="wrapper-content withtoc">
-                <div className="container">
-                    <article className="posts">
-                        <h1>{props.post.title}</h1>
-                        <div clsss="meta">
-                            <span className="date">
-                                {props.post.metas.date}
-                            </span>
-                            {props.post.metas.categories.map((category, index) => {
-                                return (
-                                    <span className="category" key={index}>
-                                        <a href={"/categories/" + category}>{category}</a>
-                                    </span>
-                                );
-                            })}
-                            <ul className="tag">
-                                {props.post.metas.tags.map((tag, index) => {
+            <section className="s-content s-content--top-padding s-content--narrow">
+                <article className="row entry format-standard">
+                    <div className="entry__media col-full">
+                        <div className="entry__post-thumb">
+                            <img src={props.post.metas.coverimage} sizes="(max-width: 2000px) 100vw, 2000px" alt="" />
+                        </div>
+                    </div>
+                    <div className="entry__header col-full">
+                        <h1 className="entry__header-title display-1">
+                            {props.post.title}
+                        </h1>
+                        <ul className="entry__header-meta">
+                            <li className="date">{props.post.metas.date}</li>
+                        </ul>
+                    </div>
+
+                    <div className="col-full entry__main">
+                        {props.content}
+                        <div className="entry__taxonomies">
+                            <div className="entry__cat">
+                                <h5>Posted In: </h5>
+                                {props.post.metas.categories.map((category, index) => {
                                     return (
-                                        <li key={index}>
-                                            <a href="/">
-                                                {tag}
-                                            </a>
-                                        </li>
+                                        <span className="entry__tax-list" key={index}>
+                                            <a href={"/categories/" + category}>{category.toUpperCase()}</a>
+                                        </span>
                                     );
                                 })}
 
-                            </ul>
+                            </div>
+
+                            <div className="entry__tags">
+                                <h5>Tags: </h5>
+                                <span className="entry__tax-list entry__tax-list--pill">
+                                    {props.post.metas.tags.map((tag, index) => {
+                                        return (
+                                            <a key={index} href={"/?tags=" + tag}>
+                                                {tag}
+                                            </a>
+                                        );
+                                    })}
+                                </span>
+                            </div>
                         </div>
-                        <div className="entry">
-                            {props.content}
+                    </div>
+                </article>
+
+                <div class="comments-wrap">
+
+                    <div id="comments" class="row">
+                        <div class="col-full">
+                            {props.disqus}
                         </div>
-                        {props.disqus}
-                    </article>
+                    </div>
                 </div>
-            </div>
+            </section>
         </React.Fragment>
     );
 }
 
 
-export default PostTemplate
+export default PostTemplate;
--- templates/scripts.js
+++ templates/scripts.js
@@ -1,8 +1,16 @@
 import React from "react";
+import "./css/base.css";
+import "./css/vendor.css";
+import "./css/main.css";
+import "./js/modernizr.js";
+import "./js/plugins.js";
+import "./js/main.js";
 import "./style.css";
 
+
+
 const ScriptsTemplate = () => {
-    return (<span></span> );
+    return (<React.Fragment></React.Fragment>);
 }
 
 
