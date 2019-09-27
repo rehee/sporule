985e6cf13111ae522b6296e783aafeedc5026616
hao <haleluohao@gmail.com>
2019-09-27 16:39:24 +0100

    fixed small bug of gh-pages

--- webpack.config.git.js
+++ webpack.config.git.js
@@ -12,7 +12,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const SitemapPlugin = require('sitemap-webpack-plugin').default;
 const Config = require("./_config");
 
-let route = "https://raw.githubusercontent.com/"+process.env.GITHUB_REPOSITORY+"/gh-pages";
+let route = "https://raw.githubusercontent.com/"+process.env.GITHUB_REPOSITORY+"/gh-pages/";
 
 console.log(route);
 
